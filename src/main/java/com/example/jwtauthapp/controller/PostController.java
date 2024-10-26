4b96d2a92cf5277aa7e91e067e6445dfaedac49e
real_issa <isanur0202@gmail.com>
2024-10-26 13:07:52 +0500

    done

--- src/main/java/com/example/jwtauthapp/controller/PostController.java
+++ src/main/java/com/example/jwtauthapp/controller/PostController.java
@@ -3,14 +3,11 @@ package com.example.jwtauthapp.controller;
 import com.example.jwtauthapp.entity.Post;
 import com.example.jwtauthapp.security.CustomUserDetails;
 import com.example.jwtauthapp.service.PostService;
-import jakarta.persistence.Id;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ public class PostController {
 
     private final PostService postService;
 
-    @GetMapping("/MyPosts")
+    @GetMapping("/my_posts")
     public ResponseEntity<List<Post>> getAllMyPosts(@AuthenticationPrincipal CustomUserDetails userDetails) {
         Long userId = userDetails.getUser().getId(); // Получаем ID пользователя
         System.out.println(userDetails.getUser());
--- src/main/java/com/example/jwtauthapp/service/AuthService.java
+++ src/main/java/com/example/jwtauthapp/service/AuthService.java
@@ -11,7 +11,6 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
